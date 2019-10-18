7a2644a2ceede61db3150224e6c34f53eff6f45c
Massimiliano Adamo <maxadamo@gmail.com>
2019-10-18 17:54:09 +0200

    add packages to install

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,8 +4,12 @@
 class zfs_nas::params {
 
   $zfs_package = $facts['os']['name'] ? {
-    'Ubuntu' => 'zfsutils-linux',
-    'CentOS' => 'zfs'
+    'Ubuntu' => [
+      'zfsutils-linux', 'zfs-auto-snapshot',
+      'debhelper', 'libcapture-tiny-perl',
+      'libconfig-inifiles-perl', 'pv', 'lzop',
+      'mbuffer'],
+    'CentOS' => ['zfs', 'zfs-auto-snapshot']
   }
 
 }
