7454cbb2e841363ccdd174ea7e12685ebe2b8a8b
Massimiliano Adamo <maxadamo@gmail.com>
2020-03-16 16:46:21 +0100

    test

--- manifests/firewall/nfs.pp
+++ manifests/firewall/nfs.pp
@@ -9,13 +9,11 @@ class zfs_nas::firewall::nfs (
   # create an array with all the clients IPs
   $clients_array = $zfs_shares.map |$items, $values| { $values['client_list'] }
   $joined_array = join($clients_array)
-  #$cleaned_array = regsubst($joined_array, /\((.+?)\)/, 'SEP', 'G')
   $_cleaned_array = regsubst($joined_array, /\,(.+?)\=@/, 'SEP', 'G')
   $cleaned_array = regsubst($_cleaned_array, /^.*=@|\,.*$/, '')
 
   $ip_array = split($cleaned_array, 'SEP')
 
-  #rw=@83.97.93.24,sec=insecure,async,no_root_squash,no_subtree_checkrw=83.97.93.25,
 
   #echo { "clients_array: ${clients_array}":; }
   #echo { "joined_array: ${joined_array}":; }
