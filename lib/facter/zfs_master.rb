c89857014b8d577b5afd8f950aa85c9edc219107
Massimiliano Adamo <maxadamo@gmail.com>
2020-03-17 17:32:25 +0100

    silent grep

--- lib/facter/zfs_master.rb
+++ lib/facter/zfs_master.rb
@@ -1,7 +1,7 @@
 Facter.add(:zfs_master) do
   setcode do
     begin
-      Facter::Core::Execution.execute('ip address show | grep secondary')
+      Facter::Core::Execution.execute('ip address show | grep -q secondary')
       true
     rescue Facter::Core::Execution::ExecutionFailure
       false
