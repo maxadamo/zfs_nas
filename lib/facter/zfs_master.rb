94f5e81d8946053efd9fc3718b53420c8636444e
Massimiliano Adamo <maxadamo@gmail.com>
2020-03-17 17:52:47 +0100

    fi fact

--- lib/facter/zfs_master.rb
+++ lib/facter/zfs_master.rb
@@ -2,7 +2,7 @@ Facter.add(:zfs_master) do
   setcode do
     begin
       masterstatus = Facter.value('ip address show | grep secondary').instance_eval do
-        if masterstatus.include? "secondary"
+        if masterstatus =~ /bcdsecondary/
           true
         else
           false
