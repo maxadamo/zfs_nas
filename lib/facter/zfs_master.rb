c108d42023a1333ba6b98e4f5d1fb41f77f3f04b
Massimiliano Adamo <maxadamo@gmail.com>
2020-03-17 19:19:44 +0100

    fix against rubocop

--- lib/facter/zfs_master.rb
+++ lib/facter/zfs_master.rb
@@ -1,8 +1,8 @@
 Facter.add(:zfs_master) do
   setcode do
     begin
-      masterstatus = Facter::Util::Resolution.exec("ip address show | grep secondary")
-      if masterstatus =~ /secondary/
+      masterstatus = Facter::Util::Resolution.exec('ip address show | grep secondary')
+      if masterstatus =~ %r{secondary}
         true
       else
         false
