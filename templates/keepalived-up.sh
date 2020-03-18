0cafe48574ef4c6c47da51dbcbba558d3ea8f10a
Massimiliano Adamo <maxadamo@gmail.com>
2020-03-18 12:19:55 +0100

    rename template

--- templates/keepalived-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-systemctl reload nfs-server.service
-
-for SHARE in <%= $zfs_share_list %>; do
-    flock /tmp/syncoid_${SHARE} syncoid --quiet root@<%= $peer_fqdn[0] %>:zfs_nas/${SHARE} zfs_nas/${SHARE} || echo "ERROR synchronizing ${SHARE}"
-done
